88797a877be9a0429046e0dade056741254dfad9
ABU BAKKAR SIDDIQUE  EMON <iotandrobotics@gmail.com>
2025-04-22 16:35:51 +0600

    class 4, post and comment add by following github repogitory
    class 4, post and comment add by following github repogitory link: https://gist.github.com/ImAbuSayed/c6daa435d17ab5ac6e05abf606233b94

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
 
 Route::get('/', function () {
@@ -16,5 +18,22 @@ Route::get('/blogs', [BlogController::class,'index'])->name('blogs.index');
 Route::get('/blogs/create', [BlogController::class,'create'])->name('blogs.create');    
 Route::post('/blogs/store', [BlogController::class,'store'])->name('blogs.store');
 Route::get('/blogs/{id}', [BlogController::class, 'show'])->name('blogs.show');
+
+
+// Homepage - Show all posts
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+// Post Routes (using resource controller)
+Route::resource('posts', PostController::class)->except(['index']); // 'index' is already defined above
+
+// Comment Routes
+// Store a new comment for a specific post
+Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+// Show edit form for a specific comment
+Route::get('/comments/{comment}/edit', [CommentController::class, 'edit'])->name('comments.edit');
+// Update a specific comment
+Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
+// Delete a specific comment
+Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
 
 
